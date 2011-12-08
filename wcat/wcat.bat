3238d567bf42b3b839ca564002bb741b3a6959b7
DamianEdwards <damian@damianedwards.com>
2011-12-08 09:32:02 -0800

    Added WCAT files for generating load using the WCAT tool.

---
+++ wcat/wcat.bat
@@ -0,0 +1,3 @@
+REM pushd %~dp0
+"c:\program files\wcat\wcat.wsf" -terminate -run -clients localhost -f settings.ubr -t shaft.ubr -s localhost -singleip -o C:\Source\github\SignalR\crank\wcat\output
+pause
